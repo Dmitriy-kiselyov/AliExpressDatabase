49137c033243129a0d65b4e494b24e6273d8559a
Dmitriy Kiselev <dmitriy.penetrator@gmail.com>
2020-01-14 09:46:07 +0300

    Уровень изоляции

--- resources/buy.sql
+++ resources/buy.sql
@@ -1,4 +1,4 @@
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 START TRANSACTION;
 UPDATE ali_express.покупатели SET баланс = баланс - (SELECT цена FROM ali_express.товары WHERE ид = {goodId}) * {count} WHERE ид = {customerId};
 INSERT INTO ali_express.покупки VALUES (NULL, {customerId}, CURDATE(), NULL, {goodId}, {count}, 'проверяется');
